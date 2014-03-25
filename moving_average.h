416958b6788ea01a7aae6573dc945d006457b8e7
Nicolas Hurtubise <316k@legtux.org>
2014-03-25 12:59:28 -0400

    Adds a moving average class (to smooth values)

---
+++ moving_average.h
@@ -0,0 +1,128 @@
+#ifndef MovingAverage_H
+#define MovingAverage_H
+
+#define SIZE 25
+
+class MovingAverage {
+
+  private:
+    int values[SIZE];
+    int size;
+
+  public:
+    /**
+     * Constructor
+     */
+    MovingAverage() {
+        size = SIZE;
+        for(int i = 0; i < size; i++) {
+            values[i] = 0;
+        }
+    }
+
+    /**
+     * @return the average of the stored values.
+     */
+    int average() {
+        int total = 0;
+        int i;
+
+        for(i = 0; i < used_size(); i++) {
+            total += values[i];
+        }
+
+        return total / i;
+    }
+
+    int median() {
+        int max = 0;
+        int min = 0;
+
+        for(int i = 0; i < size && values[i] != 0; i++) {
+            min = (min == 0 || values[i] < min ? values[i] : min);
+            max = (max == 0 || values[i] > max ? values[i] : max);
+        }
+
+        return (max + min)>>1; // bitshift to divide per two 
+    }
+
+    int max() {
+        int max = 0;
+
+        for(int i = 0; i < used_size(); i++) {
+            max = (max == 0 || values[i] > max ? values[i] : max);
+        }
+
+        return max;
+    }
+
+    /**
+     * @return the minimum value.
+     */
+    int min() {
+        int min = 0;
+
+        for(int i = 0; i < used_size() && values[i] != 0; i++) {
+            min = (min == 0 || values[i] < min ? values[i] : min);
+        }
+
+        return min;
+    }
+
+    /**
+     * Adds a value in the array. Note : adding a 0 will screw up your moving
+     * average, since the first 0 is considered to be the end of the stored
+     * values.
+     *
+     * @return the position at which the value has been added.
+     */
+    int add(int value) {
+        int i = used_size() - 1;
+
+        if(i == size) {
+            // Remove the oldest value and free the last space
+            for(i = 1; i < size && values[i] != 0; i++) {
+                values[i-1] = values[i];
+            }
+        }
+
+        values[i] = value;
+
+        return i;
+    }
+
+    /**
+     * Returns the used size (aka. the number of values stored)
+     */
+    int used_size() {
+        int i;
+
+        for(i = 0; i < size && values[i] != 0; i++);
+
+        return i + 1;
+    }
+
+    /**
+     * @return the total size available
+     */
+    int available_size() {
+        return size;
+    }
+
+    /**
+     * Sets every element to zero
+     */
+    void reset() {
+        for(int i = 0; i < size; i++) {
+            values[i] = 0;
+        }
+    }
+
+    /**
+     * @return the value at position
+     */
+    int get(int position) {
+        return values[position];
+    }
+};
+#endif
